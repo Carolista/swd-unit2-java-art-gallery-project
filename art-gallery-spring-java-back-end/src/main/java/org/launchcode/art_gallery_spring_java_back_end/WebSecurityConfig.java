a0efc518a9dc0304a2f222b7ad25b71f01f861a1
Carolista <cjones.renaissancewoman@gmail.com>
2025-07-31 02:07:47 -0400

    added Spring Security dependency and WebSecurityConfig

---
+++ art-gallery-spring-java-back-end/src/main/java/org/launchcode/art_gallery_spring_java_back_end/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package org.launchcode.art_gallery_spring_java_back_end;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    // Automatically authenticate all requests except /login and /register
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/register")
+                        .permitAll().anyRequest().authenticated())
+                        .httpBasic(Customizer.withDefaults())
+                        .build();
+    }
+}
