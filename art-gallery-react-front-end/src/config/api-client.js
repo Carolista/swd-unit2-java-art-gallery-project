12581faaeeae4935df73888cb99e6c61bdb12884
Carolista <cjones.renaissancewoman@gmail.com>
2025-08-03 09:20:03 -0400

    added configuration for axios

--- art-gallery-react-front-end/src/config/api-client.js
+++ art-gallery-react-front-end/src/config/api-client.js
@@ -1,21 +1,23 @@
-import axios from "axios";
-import { AuthResponse } from "../classes/AuthResponse";
+import axios from 'axios';
+import { getTokenFromStorage } from '../services/storage-service';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
-})
+	baseURL: 'http://localhost:8080/api',
+});
 
-// apiClient.interceptors.request.use(config => {
-//     if (!config.url?.includes("/login") && !config.url?.includes("/register")) {
-//         const authObject = localStorage.getItem("user");
-//         if (authObject) {
-//             console.log('authObject', authObject);
-//             // let response = new AuthResponse(...authObject);
-//             const {token} = JSON.parse(authObject);
-//             config.headers.Authorization = `Bearer ${token}`;
-//         }
-//     }
-//     return config;
-// }, error => Promise.reject(error));
+// Ensures existing token is sent along with requests other than /login and /register
+// But currently this doesn't matter because my /admin requests don't use axios
+apiClient.interceptors.request.use(
+	config => {
+		if (!config.url?.includes('/login') && !config.url?.includes('/register')) {
+			const token = getTokenFromStorage();
+			if (token) {
+				config.headers.Authorization = `Bearer ${token}`;
+			}
+		}
+		return config;
+	},
+	error => Promise.reject(error)
+);
 
 export default apiClient;
