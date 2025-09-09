d314a6a27a771460f20effe64ef104d6d7d9033f
Carolista <cjones.renaissancewoman@gmail.com>
2025-09-09 05:54:50 -0400

    updated comments and .gitignore

--- art-gallery-backend/src/main/java/org/launchcode/art_gallery_backend/controllers/ArtworkController.java
+++ art-gallery-backend/src/main/java/org/launchcode/art_gallery_backend/controllers/ArtworkController.java
@@ -10,25 +10,28 @@ import java.util.Collection;
 @RequestMapping("/api/artworks")
 public class ArtworkController {
 
-    // Corresponds to http://localhost:8080/api/artworks
+    // Retrieve all artworks from database
+    // GET http://localhost:8080/api/artworks/
     @GetMapping("")
     public Collection<Artwork> getAllArtworks() {
         return ArtworkData.getAll();
     }
 
-    // Use a query parameter for dynamic results
-    // Corresponds to http://localhost:8080/api/artworks/add?title=The+Starry+Night&artist=Vincent+van+Gogh (for example)
+    // Use a path parameter for dynamic results
+    // Corresponds to http://localhost:8080/api/artworks/details/3 (for example)
+    @GetMapping("/details/{artworkId}")
+    public Artwork displayArtworkDetails(@PathVariable(value = "artworkId") int artworkId) {
+        return ArtworkData.getById(artworkId);
+    }
+
+    // Save new artwork to database
+    // Uses query parameters for dynamic results
+    // POST http://localhost:8080/api/artworks/add?title=The+Starry+Night&artist=Vincent+van+Gogh (for example)
     @PostMapping("/add")
-    public String addNewArtwork(@RequestParam(value="title") String title, @RequestParam(value="artist") String artist) {
+    public String addNewArtwork(@RequestParam(value = "title") String title, @RequestParam(value = "artist") String artist) {
         Artwork newArtwork = new Artwork(title, artist);
         ArtworkData.addNew(newArtwork);
         return "Artwork added: " + newArtwork;
     }
 
-    // Use a path parameter for dynamic results
-    // Corresponds to http://localhost:8080/api/artworks/details/3 (for example)
-    @GetMapping("/details/{artworkId}")
-    public Artwork displayArtworkDetails(@PathVariable(value="artworkId") int artworkId) {
-        return ArtworkData.getById(artworkId);
-    }
 }
