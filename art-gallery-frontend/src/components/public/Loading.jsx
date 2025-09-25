a5425276005814535b6c5437866b96bf6b5f03ca
Carolista <cjones.renaissancewoman@gmail.com>
2025-09-25 00:55:09 -0400

    added FontAwesome CDN & Spinner to Loading component

--- art-gallery-frontend/src/components/public/Loading.jsx
+++ art-gallery-frontend/src/components/public/Loading.jsx
@@ -1,11 +1,16 @@
-const Loading = () => {
+import Spinner from "../common/Spinner";
+
+const Loading = ({ dataName }) => {
 
     // TODO: Add spinner similar to Garden Planning Tool idea
-    
+
+    let data = dataName || "data";
+
 	return (
 		<main className="main-content">
-        <h1>Loading</h1>
-			<div>Retrieving artwork from server...</div>
+            <h1>Loading</h1>
+			<div>Retrieving {data} from server...</div>
+            <Spinner />
 		</main>
 	);
 };
