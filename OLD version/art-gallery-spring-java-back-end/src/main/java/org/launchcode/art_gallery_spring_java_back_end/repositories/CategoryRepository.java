d80c6097b35dfef3065501fa87ecd576cfed02aa
Carolista <cjones.renaissancewoman@gmail.com>
2025-09-30 15:20:41 -0400

    removed old project and updated README

--- OLD version/art-gallery-spring-java-back-end/src/main/java/org/launchcode/art_gallery_spring_java_back_end/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.launchcode.art_gallery_spring_java_back_end.repositories;
-
-import org.launchcode.art_gallery_spring_java_back_end.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
