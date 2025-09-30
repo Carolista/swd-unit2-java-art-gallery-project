050350df7a4d47b971ed7fa57ed2ce8f40488baf
Carolista <cjones.renaissancewoman@gmail.com>
2025-09-30 15:27:07 -0400

    removed old project and updated README

--- OLD version/art-gallery-spring-java-back-end/src/main/java/org/launchcode/art_gallery_spring_java_back_end/repositories/ArtistRepository.java
+++
@@ -1,7 +0,0 @@
-package org.launchcode.art_gallery_spring_java_back_end.repositories;
-
-import org.launchcode.art_gallery_spring_java_back_end.models.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtistRepository extends JpaRepository<Artist, Integer> {
-}
