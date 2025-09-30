e49112240701c59bdbc89b2b1491a46481b84e1a
Carolista <cjones.renaissancewoman@gmail.com>
2025-09-30 15:21:34 -0400

    removed old project and updated README

--- OLD version/art-gallery-react-front-end/src/components/common/exports.js
+++
@@ -1,6 +0,0 @@
-import { default as Checkbox } from './Checkbox';
-import { default as InputErrorMessage } from './InputErrorMsg';
-import { default as Select } from './Select';
-import { default as TextInput } from './TextInput';
-
-export { Checkbox, InputErrorMessage, Select, TextInput };
