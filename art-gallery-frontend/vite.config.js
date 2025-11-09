c4840999884e97bdd4d1e4f6fdbf87cac1672ed6
Carolista <cjones.renaissancewoman@gmail.com>
2025-11-09 03:29:21 -0500

    added aliases for file paths

--- art-gallery-frontend/vite.config.js
+++ art-gallery-frontend/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+            '@classes': path.resolve(__dirname, './src/classes'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@form-input': path.resolve(__dirname, './src/components/form-input'),
+			'@config': path.resolve(__dirname, './src/config'),
+			'@context': path.resolve(__dirname, './src/context'),
+			'@services': path.resolve(__dirname, './src/services'),
+			'@shared': path.resolve(__dirname, './src/shared'),
+		},
+	},
 	server: {
 		watch: {
 			usePolling: true,
