ed72a8239c56a4c9e13b074139450d80f7a4991e
Carolista <cjones.renaissancewoman@gmail.com>
2025-11-14 01:20:46 -0500

    improved responsiveness for ArtworkDetailsPage

--- art-gallery-frontend/src/components/pages/public/artworks/ArtworksDetailsPage.jsx
+++ art-gallery-frontend/src/components/pages/public/artworks/ArtworksDetailsPage.jsx
@@ -28,66 +28,63 @@ const ArtworkDetailsPage = () => {
 		);
 	} else {
 
-        // TODO: Replace all bootstrap classes and use grid instead
 		return (
 			<Main>
 				<GoBack text={'View All Artworks'} handleClick={handleGoToArtworksPage} />
 				<h2>ARTWORK DETAILS</h2>
 				<div className='artwork-details-container'>
-					<div className='row'>
-						<div className='col-6'>
-							<img
-								className='artwork-card-image'
-								src={artwork.details.getImageURL()}
-								alt={
-									artwork.title +
-									' by ' +
-									artwork.artist.getFullName()
-								}
-							/>
-						</div>
-						<div className='col-6'>
-							<h4 className='mb-4 mt-2 font-bold'>
-								{artwork.title +
-									' (' +
-									artwork.details.yearCreated +
-									')'}
-							</h4>
-							<>
-								<h5>Artist</h5>
-								<p>
-									{artwork.artist.getFullName()} &mdash;{' '}
-									{artwork.artist.location}
-								</p>
-							</>
-							{artwork.details.description != '' && (
-								<>
-									<h5>Description</h5>
-									<p>{artwork.details.description}</p>
-								</>
-							)}
-							<>
-								<h5>
-									{artwork.categories.length === 1
-										? 'Category'
-										: 'Categories'}
-								</h5>
-								<p>{artwork.getFormattedCategories()}</p>
-							</>
-							{artwork.details.media != '' && (
-								<>
-									<h5>Media</h5>
-									<p>{artwork.details.media}</p>
-								</>
-							)}
-							{artwork.details.getDimensions() != '' && (
-								<>
-									<h5>Dimensions</h5>
-									<p>{artwork.details.getDimensions()}</p>
-								</>
-							)}
-						</div>
-					</div>
+                    <div className='artwork-details-image-container'>
+                        <img
+                            className='artwork-card-image'
+                            src={artwork.details.getImageURL()}
+                            alt={
+                                artwork.title +
+                                ' by ' +
+                                artwork.artist.getFullName()
+                            }
+                        />
+                    </div>
+                    <div className='artwork-details-text-container'>
+                        <h4 className='mb-4 mt-2 font-bold'>
+                            {artwork.title +
+                                ' (' +
+                                artwork.details.yearCreated +
+                                ')'}
+                        </h4>
+                        <>
+                            <h5>Artist</h5>
+                            <p>
+                                {artwork.artist.getFullName()} &mdash;{' '}
+                                {artwork.artist.location}
+                            </p>
+                        </>
+                        {artwork.details.description != '' && (
+                            <>
+                                <h5>Description</h5>
+                                <p>{artwork.details.description}</p>
+                            </>
+                        )}
+                        <>
+                            <h5>
+                                {artwork.categories.length === 1
+                                    ? 'Category'
+                                    : 'Categories'}
+                            </h5>
+                            <p>{artwork.getFormattedCategories()}</p>
+                        </>
+                        {artwork.details.media != '' && (
+                            <>
+                                <h5>Media</h5>
+                                <p>{artwork.details.media}</p>
+                            </>
+                        )}
+                        {artwork.details.getDimensions() != '' && (
+                            <>
+                                <h5>Dimensions</h5>
+                                <p>{artwork.details.getDimensions()}</p>
+                            </>
+                        )}
+                    </div>
 				</div>
 			</Main>
 		);
