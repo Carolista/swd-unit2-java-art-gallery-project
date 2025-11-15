92a5eaac6e1006ec1dc9174728001530ffdfd0ec
Carolista <cjones.renaissancewoman@gmail.com>
2025-11-14 20:09:32 -0500

    fixed variable value

--- art-gallery-frontend/src/components/layout/exports.js
+++ art-gallery-frontend/src/components/layout/exports.js
@@ -1,7 +1,9 @@
+import { default as DynamicNavLink } from './DynamicNavLink';
 import { default as Footer } from './Footer';
 import { default as Form } from './Form';
 import { default as Header } from './Header';
 import { default as Main } from './Main';
+import { default as MobileNavMenu } from './MobileNavMenu';
 import { default as NavMenu } from './NavMenu';
 
-export { Footer, Form, Header, Main, NavMenu };
+export { DynamicNavLink, Footer, Form, Header, Main, MobileNavMenu, NavMenu };
